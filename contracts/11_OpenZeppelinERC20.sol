e9a4191d7b6b55bb67d1b764555da84030327cda
phsinghka <phsinghka@gmail.com>
2022-09-27 23:31:31 +0000

    First Commit

---
+++ contracts/11_OpenZeppelinERC20.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Token is ERC20 {
+
+    constructor(uint initialSupply) ERC20("Piyush", "PHS") {
+        _mint(msg.sender, initialSupply);
+        
+    }
+    
+}
